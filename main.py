8ed073fc62bc83c15995d64b401561e2eded05c8
Ahmet Ataşoğlu <ahmetatasoglu98@gmail.com>
2019-06-30 22:50:42 +0300

    added files.

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Genetic Algorithms and Shakespeare 
+author: Ahmet Atasoglu
+url: 
+"""
+from time import time
+
+from classes import Monkeys
+
+max_generation = 1000
+population_size = 200
+mutation_rate = 0.01 
+phrase = "To be or not to be."
+
+monkeys = Monkeys(population_size, mutation_rate, phrase)
+
+print("1-Best_Genes\t2-Average_of_Fitness", "\n","="*50)
+
+count = 0
+start = time()
+while count < max_generation:
+    monkeys.natural_selection()
+    monkeys.generate()
+    monkeys.calc_fitness()
+    monkeys.evaluate()
+    print(monkeys.best_phrase,"\t",monkeys.get_average())
+    count +=1
+    if monkeys.is_finished:
+        end = time()
+        print("\nTerminated", "//",
+            "Generation:", str(monkeys.generation), "//",
+            "Time elapsed:", "{0:.2f}".format(end-start), "seconds")
+        break
+    
+input("Press a key to exit")
